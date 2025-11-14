6c6d76fdb61787c80dbacf8a39c7902cbe13ab32
ronni2030 <rav.villa@yavirac.edu.ec>
2025-11-14 14:32:48 -0500

    puerto

--- main.py
+++ main.py
@@ -17,5 +17,5 @@ def suma_route(a, b):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
   
